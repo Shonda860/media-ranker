4311719a68d866e39808dc391a1bc1ef32b74b31
Shonda Warren <norshonda.warren@gmail.com>
2020-05-16 17:50:50 -0700

    added strong params, edit test

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,7 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
-  
+
   def show 
     @work = Work.find_by(id: params[:id].to_i)
     if @work.nil?
@@ -10,4 +10,17 @@ class WorksController < ApplicationController
       return
     end
   end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+end
+
+private 
+def work_params 
+  return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,3 @@
 class Work < ApplicationRecord
+  validates :title, :creator, :publication_year, :description, presence: true
 end
