c860157740e71457bf66cd8145e118ddbe0af2e8
Shonda Warren <norshonda.warren@gmail.com>
2020-05-17 13:59:48 -0700

    deleted dup database and reset seeds & fixed typo

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,17 +1,12 @@
 class Work < ApplicationRecord
   validates :title, :creator, :publication_year, :description, presence: true
 
-  def self.by_category
-    categories = {"albums"=>[], "book" => [], "movie"=>[]}
+  def self.category
+    categories = {"album" => [], "book" => [], "movie" =>[]}
     categories.each do |category,record|
-      set = select{ |work| work.category == category}
+      set = all.select{ |work| work.category == category}
       categories[category] = set
       end
         return categories
-      # Work.all.where(category: "album").limit(10).sort.each do |work|
-    # work[:title]
-    # work[:creator]
-    # end
-
   end
 end
