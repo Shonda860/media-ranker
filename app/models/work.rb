dd52d9907255e114fd803331a75966ad0217d8c7
Shonda Warren <norshonda.warren@gmail.com>
2020-05-17 03:46:30 -0700

    worked on model logic for top 10

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,17 @@
 class Work < ApplicationRecord
   validates :title, :creator, :publication_year, :description, presence: true
+
+  def self.by_category
+    categories = {"albums"=>[], "book" => [], "movie"=>[]}
+    categories.each do |category,record|
+      set = select{ |work| work.category == category}
+      categories[category] = set
+      end
+        return categories
+      # Work.all.where(category: "album").limit(10).sort.each do |work|
+    # work[:title]
+    # work[:creator]
+    # end
+
+  end
 end
