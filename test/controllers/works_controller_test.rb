fff59526c6abc322d220c8661344e50d2c5b5fc5
Shonda Warren <norshonda.warren@gmail.com>
2020-05-16 17:02:15 -0700

    wrote test for works show/index

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 describe WorksController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @work = Work.create(
+      {
+        category: "Album",
+        title: "Ye",
+        creator: "Kanye West",
+        publication_year: "2018",
+        description: "xxxxxx",
+      }
+    )
+  end
+
+  describe "index" do
+    it "responds with success when there are many works saved" do
+      #Act
+      get works_path
+
+      #Assert
+      must_respond_with :success
+    end
+
+    it "can get the root path" do
+       #Act
+       get root_path
+
+       #Assert
+       must_respond_with :success
+
+  end
+  describe "show" do
+    it "can get a valid work" do 
+      #Act
+      get work_path(@work.id)
+      
+      #Assert
+      must_respond_with :success
+    end
+
+    it "can redirect if gets a invalid work" do 
+      #Act
+      get work_path(-1)
+
+      #Assert
+      must_respond_with :redirect
+    end
+  end
 end
