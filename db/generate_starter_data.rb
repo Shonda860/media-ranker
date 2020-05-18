14514fd5332dd2250b3b96f888205329088862b5
Shonda Warren <norshonda.warren@gmail.com>
2020-05-17 23:45:27 -0700

    added user seed,fix index and add to app layout

--- db/generate_starter_data.rb
+++ db/generate_starter_data.rb
@@ -10,15 +10,26 @@ require "csv"
 # $ rails db:reset
 # doesn't currently check for if titles are unique against each other
 
-CSV.open("db/works_seeds.csv", "w", :write_headers => true,
-                                    :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
-  25.times do
-    category = %w(album book).sample
-    title = Faker::Coffee.blend_name
-    creator = Faker::Name.name
-    publication_year = rand(Date.today.year - 100..Date.today.year)
-    description = Faker::Lorem.sentence
+# CSV.open("db/works_seeds.csv", "w", :write_headers => true,
+#                                     :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
+#   25.times do
+#     category = %w(album book).sample
+#     title = Faker::Coffee.blend_name
+#     creator = Faker::Name.name
+#     publication_year = rand(Date.today.year - 100..Date.today.year)
+#     description = Faker::Lorem.sentence
 
-    csv << [category, title, creator, publication_year, description]
-  end
-end
+#     csv << [category, title, creator, publication_year, description]
+#   end
+# end
+# File.open("db/users_seeds.csv", "a+",:write_headers => true,
+#     :headers => ["username", "votes", "joined"]) do |file|
+# 25.times do
+# username = Faker::FunnyName.name
+# votes = 0
+# joined = rand(Date.today - 100..Date.today)
+# file << [username, votes, joined]
+# puts "done"
+# end
+# end
+
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,57 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+   
+    describe "index" do
+      it "responds with success" do
+        #Act
+        get users_path
+  
+        #Assertra
+        must_respond_with :success
+      end
+  
+      it "can get the root path" do
+         #Act
+         get root_path
+  
+         #Assert
+         must_respond_with :success 
+      end
+    end
+    
+    describe "show" do
+      it "can get a valid work" do 
+        #Act
+        get work_path(@work.id)
+        
+        #Assert
+        must_respond_with :success
+      end
+  
+      it "can redirect if gets a invalid work" do 
+        #Act
+        get work_path(-1)
+  
+        #Assert
+        must_respond_with :redirect
+      end
+    end
+  
+    describe "edit" do 
+      it "it can edit a work from an existing work list" do
+         #Act
+        get edit_work_path(@work.id)
+        
+         #Assert
+        must_respond_with :success
+      end
+      it "response with a redirect when given invaild work id" do
+        #Act
+        get edit_work_path(-1)
+  
+        #Assert
+        must_respond_with :not_found
+      end
+    end
 end
