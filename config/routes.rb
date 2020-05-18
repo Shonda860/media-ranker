0a9815e8a67d8c89486bc5792f498ab918eda65c
Shonda Warren <norshonda.warren@gmail.com>
2020-05-17 19:54:27 -0700

    created homepagescontroller & addto routes + users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "works#index"
+  root to: "homepages#index"
   resources :works
+  resources :users
 
   post "/works" , to: "works#by_category", as: "by_category"
 end
